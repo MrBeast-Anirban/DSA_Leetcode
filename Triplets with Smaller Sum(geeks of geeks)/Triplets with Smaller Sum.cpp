296cc87e819768de411574e46dc2645fe918e575
Anirban Maitra <43218319+MrBeast-Anirban@users.noreply.github.com>
2026-03-22 20:01:44 +0530

    Add countTriplets function to calculate triplet counts
    Implement countTriplets function to count triplets with sum less than target.

--- Triplets with Smaller Sum(geeks of geeks)/Triplets with Smaller Sum.cpp
+++ Triplets with Smaller Sum(geeks of geeks)/Triplets with Smaller Sum.cpp
@@ -1,1 +1,22 @@
+class Solution {
 
+  public:
+    long long countTriplets(int n, long long target, long long nums[]) {
+        sort(nums, nums+n);
+        int count = 0;
+        for(int i = 0; i < n; i++){
+            int j = i+1, k = n-1;
+            while(j < k){
+                int sum = nums[i]+nums[j]+nums[k];
+                if(sum >= target){
+                    k--;
+                }
+                else{
+                    count += (k-j); //all k less than current k will be in the triplet
+                    j++;
+                }
+            }
+        }
+        return count;
+    }
+};
