3f0a3ec23b6b1cc2106c033187c85f84aaab9fba
Anirban Maitra <43218319+MrBeast-Anirban@users.noreply.github.com>
2026-05-20 20:43:02 +0530

    Time: 0 ms (100%), Space: 8.5 MB (63.7%) - LeetHub

--- 0844-backspace-string-compare/0844-backspace-string-compare.cpp
+++ 0844-backspace-string-compare/0844-backspace-string-compare.cpp
@@ -1,37 +1,27 @@
-// Two pointer technique.
-
 class Solution {
 public:
     bool backspaceCompare(string s, string t) {
-        int i = s.size()-1, j = t.size()-1;
-        int skips = 0, skipt = 0;
-        while(i >= 0 || j >= 0){
-            //find the position of next character in string s
-            while(i >= 0){
-                if(s[i] == '#'){
-                    skips++, i--;
+        string newS = "", newT = "";
+        int count = 0;
+        for(int i = s.length()-1; i >= 0; i--){
+            if(s[i] == '#') count++;
+            else{
+                if(count == 0) newS = s[i]+newS;
+                else{
+                    count--;
                 }
-                else if(skips > 0){
-                    skips--, i--;
-                }
-                else break;
             }
-            // find position of next character in string t
-            while(j >= 0){
-                if(t[j] == '#'){
-                    skipt++, j--;
-                }
-                else if(skipt > 0){
-                    skipt--, j--;
-                }
-                else break;
-            }
-            //If two actual characters are different
-            if(i >= 0 and j >= 0 and s[i] != t[j]) return false;
-            //If expecting to compare the character vs nothing
-            if((i >= 0) != (j >= 0)) return false;
-            i--, j--;
         }
-        return true;
+        count = 0;
+        for(int i = t.length()-1; i >= 0; i--){
+            if(t[i] == '#') count++;
+            else{
+                if(count == 0) newT = t[i]+newT;
+                else{
+                    count--;
+                }
+            }
+        }
+        return newS==newT;
     }
 };
